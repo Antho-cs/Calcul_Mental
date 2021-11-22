f1dd24602177444fcad03486747eb9afdde54711
AlexGit2019 <56688039+AlexGit2019@users.noreply.github.com>
2021-11-22 20:28:15 +0100

    Ajout d'un commentaire

--- src/scripts_sql/script_creation_bdd_and_user.sql
+++ src/scripts_sql/script_creation_bdd_and_user.sql
@@ -4,7 +4,7 @@ use expressions_polonaises_b3;
 
 create table users(id int not null auto_increment primary key, login varchar(255) not null, hashed_passwd varchar(255) not null, constraint UC_Login unique (login));
 
-insert into users(login, hashed_passwd) values ('Alex','9af9eef31e66682c7a4c3053e2f28007fd5f6ea4f2b668263ed2716fe4c793d8');
+insert into users(login, hashed_passwd) values ('Alex','9af9eef31e66682c7a4c3053e2f28007fd5f6ea4f2b668263ed2716fe4c793d8'); --AG
 
 create user 'user_expressions_polonaises'@'localhost' identified by 'ep';
 
